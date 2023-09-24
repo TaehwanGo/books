01ab90f033399f7e7b757246098e66d0045ceec2
Tony <gth1123@naver.com>
2023-09-24 17:58:27 +0900

    함수형 16. 비동기 함수 실행 순서 맞추기

--- Grokking-Simplicity/16_타임라인-사이에-자원-공유하기/DroppingQueue/main.js
+++ Grokking-Simplicity/16_타임라인-사이에-자원-공유하기/DroppingQueue/main.js
@@ -20,12 +20,45 @@ const button10 = document.querySelector(".button10");
 
 const button100 = document.querySelector(".button100");
 
-button10.addEventListener("click", async () => {
+const button10Callback = async () => {
   await slowReturn();
   showBox.innerHTML = "10";
-});
+};
 
-button100.addEventListener("click", async () => {
+const button100Callback = async () => {
   await fastReturn();
   showBox.innerHTML = "100";
-});
+};
+
+// 큐 관련 코드 시작
+let queue = [];
+let isProcessing = false;
+const MAX_QUEUE_SIZE = 5;
+
+const processQueue = async () => {
+  if (isProcessing) return;
+  isProcessing = true;
+
+  while (queue.length > 0) {
+    const task = queue.shift();
+    await task();
+  }
+
+  isProcessing = false;
+};
+
+const enqueueTask = (task) => {
+  if (queue.length < MAX_QUEUE_SIZE) {
+    queue.push(task);
+    processQueue();
+  } else {
+    console.warn("Queue is full! Task is not added.");
+  }
+};
+// 큐 관련 코드 끝
+
+// button10.addEventListener("click", button10Callback);
+// button100.addEventListener("click", button100Callback);
+
+button10.addEventListener("click", () => enqueueTask(button10Callback));
+button100.addEventListener("click", () => enqueueTask(button100Callback));
